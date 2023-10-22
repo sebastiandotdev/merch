eff56bc331c63416ffbcad2a66e5a55ff2a1a30d
castrogarciajs <johancs.mm@gmail.com>
2023-10-21 23:00:36 -0500

    test: add example test

---
+++ test/Playground.spec.tsx
@@ -0,0 +1,8 @@
+import {render, screen, within} from '@testing-library/react'
+
+
+test('Example', () => {
+    render(<div>Example</div>)
+    const element = screen.getByText('Example')
+    expect(element).toBeDefined()
+})
