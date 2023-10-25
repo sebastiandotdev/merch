a560222af9a6ef0390562400ae06fd479f7df480
castrogarciajs <johancs.mm@gmail.com>
2023-10-25 09:30:34 -0500

    feat: add utils

---
+++ lib/utils.ts
@@ -0,0 +1,19 @@
+const navbarMerch = [
+  {
+    id: 1,
+    label: 'Home',
+    href: '/',
+  },
+  {
+    id: 2,
+    label: 'Catalog',
+    href: '/catalog',
+  },
+  {
+    id: 3,
+    label: 'Contact',
+    href: '/contact',
+  },
+]
+
+export { navbarMerch }
