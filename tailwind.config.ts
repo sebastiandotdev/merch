ad0e7eca5e267c973f5074e8dbb2e0b1d514abc8
castrogarciajs <johancs.mm@gmail.com>
2023-10-25 09:29:51 -0500

    style: format code according style guid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,21 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'hero-pattern': 'url("/background-pet-cat.jpg")',
-            },
-        },
-        fontFamily: {
-            amiko: ['Amiko', 'sans-serif'],
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': 'url("/background-pet-cat.jpg")',
+      },
     },
-    plugins: [],
+    fontFamily: {
+      amiko: ['Amiko', 'sans-serif'],
+    },
+  },
+  plugins: [],
 }
 export default config
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'happy-dom',
-        globals: true,
-    },
+  plugins: [react()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+  },
 })
