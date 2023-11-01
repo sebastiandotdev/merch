0258cee6e100d22d1284ad79b39c0f68520e7bc1
castrogarciajs <johancs.mm@gmail.com>
2023-11-01 11:14:26 -0500

    fix: add folder styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import '@/public/globals.css'
+import '../public/styles/globals.css'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
