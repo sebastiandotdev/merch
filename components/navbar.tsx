72dbaec04c7c7a2766a1dbaf83adcb9d09deeffd
castrogarciajs <johancs.mm@gmail.com>
2023-11-01 11:23:34 -0500

    test: add testing component navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,9 @@ import { navbarMerch } from '../lib/utils'
 
 export default function Navbar() {
   return (
-    <ul className='flex justify-center items-center gap-5 mx-7'>
+    <ul className='flex justify-center items-center gap-5 mx-7' role='list'>
       {navbarMerch.map((link) => (
-        <li key={link.id}>
+        <li key={link.id} id='li'>
           <Link href={link.href} className='hover:underline font-amiko'>
             {link.label}
           </Link>
