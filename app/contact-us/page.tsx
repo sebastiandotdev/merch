57e34318f3caaff4fcfd974c9a2f8070b510745c
castrogarciajs <johancs.mm@gmail.com>
2023-11-01 13:19:07 -0500

    styles: add styles form

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,58 +1,66 @@
 export default function ContactUs() {
   return (
-    <>
-      <section className='w-full flex  justify-center items-center  '>
-        <form action=''>
-          <div className='container w-max border-2 border-black py-8 px-8'>
-            <h1 className='text-3xl mb-5 text-center'> Información Personal</h1>
-            <div className='flex gap-3 mb-4'>
-              <label htmlFor='name'>Nombre:</label>
-              <input
-                type='text'
-                name='name'
-                placeholder='Escribe tu nombre'
-                className=' border-2 border-black rounded px-2'
-                required
-              />
-            </div>
-
-            <div className='flex gap-3 mb-4'>
-              <label htmlFor='tel'>Teléfono:</label>
-              <input
-                type='tel'
-                name='tel'
-                pattern='[0-9]{9}'
-                placeholder='Dejanos tu numero de telefono'
-                className='border-2 border-black rounded px-2'
-                required
-              />
-            </div>
-
-            <div className='flex gap-3 mb-4'>
-              <label htmlFor='message'>Mensaje:</label>
-              <textarea
-                name=''
-                id=''
-                placeholder='Dejanos tu mensaje o sugerencia'
-                className='border-2 border-black rounded px-2'
-                cols={22}
-                required
-                defaultValue='Borra este mensaje'
-              ></textarea>
-            </div>
-
-            <div className='flex justify-center mb-4'>
-              <button
-                type='submit'
-                className='rounded border-2 bg-blue-700 text-white p-4 flex mb-3 '
-              >
-                Enviar datos
-              </button>
-            </div>
-            <h3>Dejanos tus datos para comunicarnos contigo!</h3>
-          </div>
-        </form>
-      </section>
-    </>
+    <section className='w-11/12 max-w-5xl mx-auto pt-4'>
+      <header className='px-10'>
+        <h2 className='text-[4.8rem] font-amiko mb-[3rem]'>Contact</h2>
+      </header>
+      <form action='#' className='px-10'>
+        <div className='grid grid-cols-2 gap-[2rem] w-full'>
+          <label
+            htmlFor='name'
+            className='w-full border border-[#121212] mb-[2rem] p-3'
+          >
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              className='w-full outline-none'
+            />
+          </label>
+          <label
+            htmlFor='email'
+            className='w-full border border-[#121212] mb-[2rem] p-3'
+          >
+            <input
+              type='email'
+              placeholder='Email'
+              name='email'
+              className='w-full outline-none'
+            />
+          </label>
+        </div>
+        <div className='flex w-full'>
+          <label
+            htmlFor='phone'
+            className='w-full border border-[#121212] mb-[2rem] p-3 flex-1'
+          >
+            <input
+              type='text'
+              placeholder='Phone number'
+              name='phone'
+              className='w-full outline-none'
+            />
+          </label>
+        </div>
+        <div className='flex w-full'>
+          <label
+            htmlFor='message'
+            className='w-full border border-[#121212] mb-[2rem] p-3 flex-1'
+          >
+            <textarea
+              rows={10}
+              placeholder='Comment'
+              name='message'
+              className='w-full outline-none resize-none'
+            />
+          </label>
+        </div>
+        <input
+          type='submit'
+          value='Send'
+          className='bg-black text-white py-3 px-[3rem] min-h-[calc(4.5rem + 1px * 2)] hover:cursor-pointer hover:opacity-90'
+        />
+      </form>
+    </section>
   )
 }
