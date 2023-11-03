41a5175aa9b7e1c396d20ce826947ccef0de889a
castrogarciajs <johancs.mm@gmail.com>
2023-11-03 09:47:30 -0500

    test: add tests for Footer component

---
+++ test/components/footer.spec.tsx
@@ -0,0 +1,20 @@
+import Footer from '../../components/footer'
+import { render, screen } from '@testing-library/react'
+
+test('should render the footer', () => {
+  render(<Footer />)
+  const footer = screen.queryByRole('contentinfo') as HTMLElement
+  expect(footer.tagName.toLowerCase()).toBe('footer')
+  expect(footer.textContent).toContain('Powered by labs')
+  expect(footer.textContent).toContain('Source')
+  expect(footer.textContent).toContain('Made with Nextjs')
+  expect(footer.className).toContain('flex')
+})
+
+test('should render nodes children', () => {
+  render(<Footer />)
+  const footer = screen.queryByRole('contentinfo') as HTMLElement
+  expect(footer.children.length).toBe(2)
+  expect(footer.children[0].tagName.toLowerCase()).toBe('span')
+  expect(footer.children[1].tagName.toLowerCase()).toBe('a')
+})
