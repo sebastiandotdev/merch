b4bc41329ff85e6331059e3394cfb84bb8b579d5
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-04 23:33:33 -0500

    feat: add component client render

---
+++ app/get-merchs.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Data } from '../lib/types'
+import CardBuy from '../components/card-buy'
+
+export default function GetMerchs() {
+  const [merchs, setMerchs] = useState<Data[]>([])
+  useEffect(() => {
+    const getMerchs = async () => {
+      try {
+        const res = await fetch('https://merch-inky.vercel.app/api')
+        const json = await res.json()
+
+        setMerchs(json.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        throw error
+      }
+    }
+    getMerchs()
+  }, [])
+  return (
+    <>
+      {merchs.map((merch) => (
+        <CardBuy
+          key={merch.id}
+          name={merch.title}
+          image={merch.photo}
+          price={merch.price}
+          description={merch.description}
+        />
+      ))}
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
 import Link from 'next/link'
-import CardBuy from '../components/card-buy'
-import { Data } from '../lib/types'
-
+import GetMerchs from './get-merchs'
 
 export default async function HomePage() {
-  const getMerchs = async <T = [],>(): Promise<T> => {
-    try {
-      const res = await fetch('https://merch-inky.vercel.app/api')
-      const json = await res.json()
-  
-      return json.data
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      throw error
-    }
-  }
-  const merchs = await getMerchs<Data[]>()
-
   return (
     <>
       <section className='h-[50vh] lg:h-screen bg-hero-pattern bg-cover bg-no-repeat relative flex items-start lg:items-center justify-start'>
@@ -38,15 +23,7 @@ export default async function HomePage() {
           Featured Products
         </h2>
         <div className='grid grid-cols-1 gap-8 sm:!gap-x-10 sm:!grid-cols-2 lg:!grid-cols-3 lg:!gap-x-12 lg:!gap-y-10'>
-          {merchs.map((merch) => (
-            <CardBuy
-              key={merch.id}
-              name={merch.title}
-              image={merch.photo}
-              price={merch.price}
-              description={merch.description}
-            />
-          ))}
+          <GetMerchs />
         </div>
       </section>
     </>
