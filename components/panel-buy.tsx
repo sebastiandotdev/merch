a69e920a6e9f4985d1b87fb5275634e3c9adcafd
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-05 20:52:03 -0500

    feat: add example product cart

--- components/panel-buy.tsx
+++ components/panel-buy.tsx
@@ -3,11 +3,40 @@
 import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { CloseModal } from './icons'
+import Image from 'next/image'
+import exampleImage from '../public/example-photo.jpg'
+import Link from 'next/link'
 
 type PanelBuyProps = {
   open: boolean
   setOpen: (open: boolean) => void
 }
+
+const products = [
+  {
+    id: 1,
+    name: 'Throwback Hip Bag',
+    href: '#',
+    color: 'Salmon',
+    price: '$90.00',
+    quantity: 1,
+    imageSrc: exampleImage,
+    imageAlt:
+      'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+  },
+  {
+    id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc: exampleImage,
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+]
+
 export default function PanelBuy({ open, setOpen }: PanelBuyProps) {
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -36,36 +65,111 @@ export default function PanelBuy({ open, setOpen }: PanelBuyProps) {
                 leaveFrom='translate-x-0'
                 leaveTo='translate-x-full'
               >
-                <Dialog.Panel className='pointer-events-auto relative w-screen max-w-md'>
-                  <Transition.Child
-                    as={Fragment}
-                    enter='ease-in-out duration-500'
-                    enterFrom='opacity-0'
-                    enterTo='opacity-100'
-                    leave='ease-in-out duration-500'
-                    leaveFrom='opacity-100'
-                    leaveTo='opacity-0'
-                  >
-                    <div className='absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4'>
-                      <button
-                        type='button'
-                        className='relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white'
-                        onClick={() => setOpen(false)}
-                      >
-                        <span className='absolute -inset-2.5' />
-                        <span className='sr-only'>Close panel</span>
-                        <CloseModal />
-                      </button>
+                <Dialog.Panel className='pointer-events-auto w-screen max-w-md'>
+                  <div className='flex h-full flex-col overflow-y-scroll bg-white shadow-xl'>
+                    <div className='flex-1 overflow-y-auto px-4 py-6 sm:px-6'>
+                      <div className='flex items-start justify-between'>
+                        <Dialog.Title className='text-lg font-medium text-gray-900 font-amiko'>
+                          Shopping cart
+                        </Dialog.Title>
+                        <div className='ml-3 flex h-7 items-center'>
+                          <button
+                            type='button'
+                            className='relative -m-2 p-2 text-gray-400 hover:text-gray-500'
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className='absolute -inset-0.5' />
+                            <span className='sr-only'>Close panel</span>
+                            <CloseModal />
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className='mt-8'>
+                        <div className='flow-root'>
+                          <ul
+                            role='list'
+                            className='-my-6 divide-y divide-gray-200'
+                          >
+                            {products.map((product) => (
+                              <li key={product.id} className='flex py-6'>
+                                <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                                  <Image
+                                    src={product.imageSrc}
+                                    alt={product.imageAlt}
+                                    className='h-full w-full object-cover object-center'
+                                    width={200}
+                                    height={200}
+                                  />
+                                </div>
+
+                                <div className='ml-4 flex flex-1 flex-col'>
+                                  <div>
+                                    <div className='flex justify-between text-base font-medium text-gray-900'>
+                                      <h3 className='font-amiko'>
+                                        <Link href={product.href}>
+                                          {product.name}
+                                        </Link>
+                                      </h3>
+                                      <p className='ml-4 font-amiko'>
+                                        {product.price}
+                                      </p>
+                                    </div>
+                                    <p className='mt-1 text-sm text-gray-500 font-amiko'>
+                                      {product.color}
+                                    </p>
+                                  </div>
+                                  <div className='flex flex-1 items-end justify-between text-sm'>
+                                    <p className='text-gray-500'>
+                                      Qty {product.quantity}
+                                    </p>
+
+                                    <div className='flex'>
+                                      <button
+                                        type='button'
+                                        className='font-medium font-amiko hover:text-red-500'
+                                      >
+                                        Remove
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
                     </div>
-                  </Transition.Child>
-                  <div className='flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl'>
-                    <div className='px-4 sm:px-6'>
-                      <Dialog.Title className='text-base font-semibold leading-6 text-gray-900 font-amiko'>
-                        Shopping Cart
-                      </Dialog.Title>
-                    </div>
-                    <div className='relative mt-6 flex-1 px-4 sm:px-6'>
-                      {/* Your content */}
+
+                    <div className='border-t border-gray-200 px-4 py-6 sm:px-6'>
+                      <div className='flex justify-between text-base font-medium text-gray-900'>
+                        <p className='font-amiko'>Subtotal</p>
+                        <p className='font-amiko'>$262.00</p>
+                      </div>
+                      <p className='mt-0.5 text-sm text-gray-500 font-amiko'>
+                        Shipping and taxes calculated at checkout.
+                      </p>
+                      <div className='mt-6'>
+                        <Link
+                          href='#'
+                          className='flex items-center justify-center rounded-md border border-transparent bg-zinc-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-zinc-700 font-amiko'
+                        >
+                          Checkout
+                        </Link>
+                      </div>
+                      <div className='mt-6 flex justify-center text-center text-sm text-gray-500'>
+                        <p className='font-amiko'>
+                          or{' '}
+                          <button
+                            type='button'
+                            className='font-medium text-zinc-600 hover:text-zinc-500'
+                            onClick={() => setOpen(false)}
+                          >
+                            Continue Shopping
+                            <span aria-hidden='true'> &rarr;</span>
+                          </button>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </Dialog.Panel>
