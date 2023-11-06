52cf9b71eac52c9b7f31bd00bda3352ffde06c9c
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-05 21:18:01 -0500

    chore: add coverage & ui config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      reporter: ['html', 'text', 'json']
+    },
+    ui: true, 
     environment: 'happy-dom',
     globals: true,
   },
