ae656cf6e231f8ecb7cb59c971c3e16fccce0672
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-07 22:25:45 -0500

    feat: add route dinamyc

---
+++ app/product-pet/[name]/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductById() {
+  return (
+    <div>
+      <h1>ID</h1>
+    </div>
+  )
+}
--- app/product-pet/page.tsx
+++ app/product-pet/page.tsx
@@ -1,4 +1,4 @@
-function ProductById() {
+export default function ProductPage() {
   return (
     <div>
       <h1>ProductById</h1>
