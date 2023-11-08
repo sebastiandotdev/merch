bd05ce20ca6331e2d19b5685082baa257deff771
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-08 10:56:06 -0500

    feat: add new icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -96,3 +96,43 @@ export function CloseModal() {
     </svg>
   )
 }
+
+export function Add() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='icon icon-tabler icon-tabler-plus'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      strokeWidth='1'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M12 5l0 14'></path>
+      <path d='M5 12l14 0'></path>
+    </svg>
+  )
+}
+export function Minus() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='icon icon-tabler icon-tabler-minus'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      strokeWidth='1'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M5 12l14 0'></path>
+    </svg>
+  )
+}
