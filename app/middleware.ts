5687ce15017c19357d2f2d46e7203e4d1a0be7b2
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-08 21:13:01 -0500

    feat: middle redirect to login

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-    const isAdmin = false
-    if(isAdmin) {
-        console.log('LOGIN')
-    }
+  const isAdmin = false
+  if (isAdmin) {
+    console.log('LOGIN')
+  }
   return NextResponse.redirect('/login')
 }
 
