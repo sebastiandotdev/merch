e925118b37af7b664bf5416c52677b13b673523b
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-08 21:11:42 -0500

    feat: add middle for admin

---
+++ app/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+    const isAdmin = false
+    if(isAdmin) {
+        console.log('LOGIN')
+    }
+  return NextResponse.redirect('/login')
+}
+
+export const config = {
+  matcher: ['/admin'],
+}
