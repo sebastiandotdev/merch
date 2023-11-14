f8dd9efc0e5c4490c8f62bb8eb6987838f9bc862
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-13 20:59:57 -0500

    refactor: shopping cart component to use global cart state.

--- components/shopping-cart.tsx
+++ components/shopping-cart.tsx
@@ -3,35 +3,24 @@
 import { useEffect, useState } from 'react'
 import { IconCart } from './icons'
 import PanelBuy from './panel-buy'
+import { useCart } from '../app/Provider'
 
 export default function Cart() {
   const [openPanel, setOpenPanel] = useState(false)
-  const [productsLocal, setProductsLocal] = useState([])
-  const updateProducts = () => {
-    const products = localStorage.getItem('products')
-    if (products) {
-      setProductsLocal(JSON.parse(products))
-    }
-  }
-  useEffect(() => {
-    updateProducts()
-    window.addEventListener('storage', updateProducts)
-    return () => window.removeEventListener('storage', updateProducts)
-  }, [])
+  const { cart } = useCart()
+
+  useEffect(() => {}, [cart])
+
   return (
     <div role='tooltip'>
-      <PanelBuy
-        open={openPanel}
-        setOpen={setOpenPanel}
-        products={productsLocal}
-      />
+      <PanelBuy open={openPanel} setOpen={setOpenPanel} products={cart} />
       <button
         className='flex gap-2 items-center border-2 border-gray-800 rounded-full px-5 py-1 font-semibold text-gray-800 hover:bg-gray-800 hover:text-white transition-colors duration-300'
         id='total'
         onClick={() => setOpenPanel(true)}
       >
         <IconCart />
-        {productsLocal.length}
+        {cart.length}
       </button>
       <dialog className='bg-transparent p-0 m-0 pt-[50%] sm:pt-0 sm:ml-auto max-w-full sm:max-w-lg w-full max-h-full h-full'></dialog>
     </div>
