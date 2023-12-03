147d0b6b4aaa1ff23f3b68d3928581d7cbb7d036
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-02 22:05:44 -0500

    Formatter code function

--- components/paypal.tsx
+++ components/paypal.tsx
@@ -7,9 +7,8 @@ export default function PaypalButton() {
     'AffSooJkvii99I2g7G9uf0_ywMzRLR256NWuRvtFhHIfcu7C-13gHc1DtvuV0eswtDJHKvYRsPf1URwE'
 
   const handleCreateOrderForPaypal = async () => {
+    
     const response = await fetch('/server/checkout', { method: 'POST' })
-
-    // Check if the response is empty
     if (!response.ok) {
       console.error('Error fetching data')
       return null
@@ -17,13 +16,11 @@ export default function PaypalButton() {
 
     const order = await response.json()
 
-    // Check if the parsed JSON is valid
     if (!order || typeof order !== 'object') {
       console.error('Invalid JSON response')
       return null
     }
 
-    console.log(order)
     return order.id
   }
 
