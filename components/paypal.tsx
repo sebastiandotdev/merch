1814e3c596f6d1cbd5a0a8e0fc670082655b9744
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-02 21:07:48 -0500

    Add client id for paypal

--- components/paypal.tsx
+++ components/paypal.tsx
@@ -3,8 +3,10 @@
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 export default function PaypalButton() {
+  const CLIENT_ID =
+    'AffSooJkvii99I2g7G9uf0_ywMzRLR256NWuRvtFhHIfcu7C-13gHc1DtvuV0eswtDJHKvYRsPf1URwE'
   return (
-    <PayPalScriptProvider options={{ clientId: 'test' }}>
+    <PayPalScriptProvider options={{ clientId: CLIENT_ID }}>
       <PayPalButtons style={{ color: 'blue', layout: 'horizontal' }} />
     </PayPalScriptProvider>
   )
