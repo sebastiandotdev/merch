e90789469fe57be33ccdbb04141f566ebd0794d4
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-02 22:15:52 -0500

    Add onApprove handle for paypal

--- components/paypal.tsx
+++ components/paypal.tsx
@@ -7,7 +7,6 @@ export default function PaypalButton() {
     'AffSooJkvii99I2g7G9uf0_ywMzRLR256NWuRvtFhHIfcu7C-13gHc1DtvuV0eswtDJHKvYRsPf1URwE'
 
   const handleCreateOrderForPaypal = async () => {
-    
     const response = await fetch('/server/checkout', { method: 'POST' })
     if (!response.ok) {
       console.error('Error fetching data')
@@ -23,12 +22,15 @@ export default function PaypalButton() {
 
     return order.id
   }
-
   return (
     <PayPalScriptProvider options={{ clientId: CLIENT_ID }}>
       <PayPalButtons
         style={{ color: 'blue', layout: 'horizontal' }}
         createOrder={handleCreateOrderForPaypal}
+        onApprove={async (data, actions) => {
+          console.log(data)
+          await actions.order?.capture()
+        }}
       />
     </PayPalScriptProvider>
   )
