eb6d0acb5f07e5660c8e5409dd53c7b563400308
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-03 20:44:03 -0500

    Remove header unnecesary

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,27 @@
 import Image from 'next/image'
-import petCat from '../public/not-bg-cat-pet.png'
+import petCat from '../public/alternative-logotipo.jpeg'
 import Cart from './shopping-cart'
 import Navbar from './navbar'
 import Link from 'next/link'
 
 export default function Header() {
   return (
-    <>
-      <header className='border-b border-b-gray-300 text-center p-1'>
-        <small className='font-amiko'>Welcome to our Shop</small>
-      </header>
-      <header className='bg-white shadow-md h-[100px] w-full relative'>
-        <nav className='w-11/12 h-24 max-w-5xl mx-auto flex items-center justify-between relative flex-col lg:flex-row'>
-          <div className='flex items-center gap-x-4'>
-            <Link href='/'>
-              <Image
-                src={petCat}
-                alt='The pet land merch'
-                className='w-auto h-12 rounded-full'
-              />
-            </Link>
-            <Navbar />
-          </div>
-          <div>
-            <Cart />
-          </div>
-        </nav>
-      </header>
-    </>
+    <header className='bg-white shadow-md h-[100px] w-full relative'>
+      <nav className='w-11/12 h-24 max-w-5xl mx-auto flex items-center justify-between relative flex-col lg:flex-row'>
+        <div className='flex items-center gap-x-4'>
+          <Link href='/'>
+            <Image
+              src={petCat}
+              alt='The pet land merch'
+              className='w-auto h-16 rounded-full'
+            />
+          </Link>
+          <Navbar />
+        </div>
+        <div>
+          <Cart />
+        </div>
+      </nav>
+    </header>
   )
 }
