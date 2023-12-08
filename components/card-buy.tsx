136b9b7bf706bec5e2226cada5f90f7a5cd25534
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-08 16:02:08 -0500

    Refactor URL product

--- components/card-buy.tsx
+++ components/card-buy.tsx
@@ -11,7 +11,10 @@ export default function CardBuy({
   description,
 }: CardBuyProps) {
   return (
-    <Link href={`/product-pet/${id}`} className='group'>
+    <Link
+      href={`/product-pet/${name.toLowerCase().replace(/ /g, '-')}-${id}`}
+      className='group'
+    >
       <header className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 relative'>
         <aside className='w-full h-full flex items-center justify-center bg-[rgba(255,255,255,0.6)] opacity-0 hover:opacity-90 transition-opacity z-10 absolute duration-500'>
           <IconCart size={30} />
