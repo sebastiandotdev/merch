dbe27533649c86943f57c7e0f0a85ae461d545fa
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-12-08 16:03:51 -0500

    Refactor code test

--- test/components/message.spec.tsx
+++ test/components/message.spec.tsx
@@ -1,5 +1,5 @@
 import Messsage from '../../components/message'
-import { screen, render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 test('should render message', () => {
   render(
