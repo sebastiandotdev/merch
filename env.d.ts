59f89c84ae95a1b44dc76b2c7be10150202bb7c0
castrogarciajs <johancs.mm@gmail.com>
2024-01-31 08:42:45 -0500

    Add declare global variable env

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_SUPABASE_URL: string
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+      NEXT_PUBLIC_PAYPAL_SECRET_KEY: string
+    }
+  }
+}
